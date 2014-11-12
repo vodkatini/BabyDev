2031e75acdb11d58c9101b44c33c7ee1dcb1b5cf
Svetoslav Georgiev <vodkatinibg@yahoo.com>
2014-11-12 14:43:22 +0200

    TopicsTemplate view added

--- BabyDev/BabyDev.Web/Areas/Administration/Controllers/TopicsController.cs
+++ BabyDev/BabyDev.Web/Areas/Administration/Controllers/TopicsController.cs
@@ -1,13 +1,14 @@
-﻿using System.Data.Entity;
-
-namespace BabyDev.Web.Areas.Administration.Controllers
+﻿namespace BabyDev.Web.Areas.Administration.Controllers
 {
     using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper;
     using AutoMapper.QueryableExtensions;
     using Kendo.Mvc.Extensions;
-    using System.Web.Mvc;
     using Kendo.Mvc.UI;
 
+    using BabyDev.Models;
     using BabyDev.Data.Contracts;
 
     using BabyDev.Web.Areas.Administration.ViewModels;
@@ -27,6 +28,47 @@ namespace BabyDev.Web.Areas.Administration.Controllers
                 .To<TopicViewModel>()
                 .ToDataSourceResult(request);
             return this.Json(result);
+        }
+
+        [HttpPost]
+        public ActionResult Create([DataSourceRequest]DataSourceRequest request, TopicViewModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var topic = new Topic();
+                Mapper.Map(model, topic, typeof(TopicViewModel), typeof(Topic));
+                this.Data.Topics.Add(topic);
+                this.Data.SaveChanges();
+            }
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, TopicViewModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var topic = this.Data.Topics.All().FirstOrDefault(t => t.Id == model.Id);
+                Mapper.Map(model, topic, typeof(TopicViewModel), typeof(Topic));
+                this.Data.Topics.Update(topic);
+                this.Data.SaveChanges();
+            }
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, TopicViewModel model)
+        {
+            if (model != null)
+            {
+                var topic = this.Data.Topics.All().FirstOrDefault(t => t.Id == model.Id);
+//                topic.Deleted = true;
+//                topic.DeletedOn = DateTime.Now;
+//                this.Data.Topics.Update(topic);
+                this.Data.Topics.Delete(topic);
+                this.Data.SaveChanges();
+            }
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
         }
     }
 }
